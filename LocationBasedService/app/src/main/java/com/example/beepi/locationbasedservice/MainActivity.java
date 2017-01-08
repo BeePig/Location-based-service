a3357673fb99b9b0fae7303c41b399ba6a71cc8c
ongnau <beepig025@gmail.com>
2017-01-09 01:29:40 +0700

    android 6 ko su dung google service client

---
+++ LocationBasedService/app/src/main/java/com/example/beepi/locationbasedservice/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.beepi.locationbasedservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+
+public class MainActivity extends AppCompatActivity {
+    private LocationClient
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
